95dade764ec2ba1b273c3bace6681d8c1e4db836
Mustafa <ajanovicmustafa14@gmail.com>
2023-04-09 11:49:40 +0200

    files

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS, PATCH');
 require 'vendor/autoload.php';
 
-Flight::route('/', function(){
-  echo 'Hello world!';
-});
+Flight::register('db', 'PDO', 
+array('mysql:host=localhost;dbname=rentacar','root',''));
 
-Flight::startt();
+
+Flight::route('GET /api/users', function(){
+  $users = Flight::db()->query('SELECT * FROM Customers', PDO::FETCH_ASSOC)->fetchAll();
+  var_dump($users);
+  Flight::json($users);
+  });
+
+
+
+
+Flight::start();
+
 ?>
+
